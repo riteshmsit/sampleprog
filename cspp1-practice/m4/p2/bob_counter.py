f9a19bc2ed4939dc70a36cbac6d8c9bee7f07cf5
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-02 17:23:39 +0530

    " -> 0 of 5 passed. pylint: -6.67/10 "

---
+++ cspp1-practice/m4/p2/bob_counter.py
@@ -0,0 +1,15 @@
+s=input("Type the string")
+m=len(s)
+b="bob"
+n=len(b)
+i=0
+c=0
+while (i<=m-n+1):
+    j=i
+    k=1
+    c1=s[j:j+3]
+    c2=b
+    if(c1==c2):
+        c=c+1
+    i=i+1
+print(c)
