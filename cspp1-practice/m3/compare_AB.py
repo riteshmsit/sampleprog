0da459ba57b2269e152ef61ddc1b5dc31a529904
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-02 15:32:59 +0530

    comparing inputs

---
+++ cspp1-practice/m3/compare_AB.py
@@ -0,0 +1,17 @@
+varA=input("Type the input for variable A")
+varB=input("Type the input for variable B")
+#for comparision between nummbers
+varA=int(varA)
+varB=int(varB)
+if(varA==varB):
+    print("equal")
+elif(varA>varB):
+    print("bigger")
+else:
+    print("smaller")
+
+#for checking if one of the inputs is a string
+#if(type(varA)==type(str) or type(varB)==type(str)):
+#    print("String involved")
+    
+
