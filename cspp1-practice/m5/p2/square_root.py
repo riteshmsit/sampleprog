4ff0e0c6d00af7551ee13350587b9e3d61711e33
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-03 22:59:04 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- cspp1-practice/m5/p2/square_root.py
+++ cspp1-practice/m5/p2/square_root.py
@@ -1,21 +1,11 @@
-# Write a python program to find the square root of the given number
-# using approximation method
-
-# testcase 1
-# input: 25
-# output: 4.999999999999998
-
-# testcase 2
-# input: 49
-# output: 6.999999999999991
-
-def main():
-	s = raw_input()
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	step = 0.1
-	# your code starts here
-
-if __name__== "__main__":
-	main()
+"""
+square root approximation
+"""
+N = int(input())
+E = 0.1
+A = 0
+A = int(A)
+while (N - (A*A) > E) and A < N:
+    if N - (A*A) > E:
+        A = A+E
+print(A)
