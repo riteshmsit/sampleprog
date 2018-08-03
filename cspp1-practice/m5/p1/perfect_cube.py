9b6c0632dda569448f882af5ceced1f5f88a15c1
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-03 22:51:59 +0530

    " -> 3 of 4 passed. pylint: 9.09/10 "

--- cspp1-practice/m5/p1/perfect_cube.py
+++ cspp1-practice/m5/p1/perfect_cube.py
@@ -1,11 +1,14 @@
+"""
+perfect cube
+"""
 GUESS = 1
 N = int(input(""))
 FLAG = 0
-while GUESS<N+1:
-    if GUESS**3==N:
+while GUESS < N+1:
+    if GUESS**3 == N:
         print(str(N) + " " + "is a perfect cube")
         FLAG = 1
         break
     GUESS = GUESS + 1
-if(FLAG == 0):
+if FLAG == 0:
     print(str(N) + " " + "is not a perfect cube")
