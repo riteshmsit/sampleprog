59d2fd373d0e9db26ea504fe3d5bf46a7d9ecac4
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-03 12:04:44 +0530

    " -> 5 of 5 passed. pylint: 9.33/10 "

--- cspp1-practice/m4/p2/bob_counter.py
+++ cspp1-practice/m4/p2/bob_counter.py
@@ -1,15 +1,15 @@
-s=input()
-m=len(s)
-b="bob"
-n=len(b)
-i=0
-c=0
-while (i<=m-n+1):
-    j=i
-    k=1
-    c1=s[j:j+3]
-    c2=b
-    if(c1==c2):
-        c=c+1
-    i=i+1
-print(c)
+S = input()
+M = len(S)
+B = "bob"
+N = len(B)
+I = 0
+C = 0
+while I <= M-N+1:
+    J = I
+    K = 1
+    C1 = S[J:J+3]
+    C2 = B
+    if C1 == C2:
+        C = C+1
+    I = I+1
+print(C)
