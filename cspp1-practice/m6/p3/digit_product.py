a73144e571c0182546ee9c11f197cb6544be0803
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-04 11:29:36 +0530

    " -> 2 of 7 passed. pylint: 10.00/10 "

--- cspp1-practice/m6/p3/digit_product.py
+++ cspp1-practice/m6/p3/digit_product.py
@@ -4,7 +4,7 @@ Third program
 N = int(input())
 I = 1
 P = 1
-if N < 0:
+if N < 0 and N != 0:
     N = abs(N)
     while N != 0:
         I = N % 10
@@ -13,7 +13,7 @@ if N < 0:
     P = P * -1
     print(P)
     N = N * -1
-if N > 0:
+if N > 0 and N != 0:
     while N != 0:
         I = N % 10
         P = P * I
