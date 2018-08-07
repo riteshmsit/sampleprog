71d1f6303d1a7a861e0f524afe9698f9ef915459
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-07 23:34:44 +0530

    guess the number

--- cspp1-practice/m5/GuessMyNumber/guess_my_number.py
+++ cspp1-practice/m5/GuessMyNumber/guess_my_number.py
@@ -1,18 +1,35 @@
-
-num = input("please think of a number between 1 to 100 and click enter")
-	print(" enter h if the given number is greater than your number , l if the given less tahn your number, else c then if it equals to your number")
-	print("is your  number 50?")
-	low = 0
-	high = 100
-	mid = ((low+high)//2)
-	while True:
-		r = input("Enter your response :")
-		if r == 'h':
-			high = mid
-		elif r == 'l':
-			low = mid
-		elif r == 'c':
-			break
-		mid = ((low+high)//2)
-		print(" is your number",mid)
-	print("your number is :",mid)
+"""
+Guess My Number
+"""
+#function
+def main():
+    print("Enter 'VAR_H' to indicate the guess is too high.")
+    print("Enter 'VAR_L' to indicate the guess is too low.")
+    print("Enter 'VAR_C' to indicate I guessed correctly.")
+    var_a = input("Enter a number between 0 and 100:")
+    var_l = 0
+    var_h = 100
+    var_ans = (var_l + var_h)//2
+    print(var_ans)
+    var_a1 = input("Enter either of VAR_H,VAR_L,VAR_C:")
+    while var_a1 != "var_c":
+        if var_a1 == "var_l":
+            var_l = var_ans
+            var_ans = (var_l+var_h)//2
+            print(var_ans)
+            print("Is your secret number"+str(var_ans)+"?")
+            print("If not Enter 'VAR_H' to indicate the guess is too high.")
+            print("Enter 'VAR_L' to indicate the guess is too low.")
+            print("Enter 'VAR_C' to indicate I guessed correctly.")
+            var_a1 = input("Enter either of VAR_H,VAR_L,VAR_C:")
+        else:
+            var_h = var_ans
+            var_ans = (var_l+var_h)//2
+            print(var_ans)
+            print("Is your secret number"+str(var_ans)+"?")
+            print("If not Enter 'VAR_H' to indicate the guess is too high.")
+            print("Enter 'VAR_L' to indicate the guess is too low.")
+            print("Enter 'VAR_C' to indicate I guessed correctly.")
+            var_a1 = input("Enter either of VAR_H,VAR_L,VAR_C:")
+    print("Your guessed number is:", var_a)
+main()
