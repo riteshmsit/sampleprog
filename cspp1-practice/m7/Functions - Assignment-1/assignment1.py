f10c65bd84fd51b46b25fcdec6f4e233de7e9492
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-07 22:30:30 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- cspp1-practice/m7/Functions - Assignment-1/assignment1.py
+++ cspp1-practice/m7/Functions - Assignment-1/assignment1.py
@@ -1,46 +1,23 @@
-# credit card company each month.
-
-# The following variables contain values as described below:
-# balance - the outstanding balance on the credit card
-# annualInterestRate - annual interest rate as a decimal
-# monthlyPaymentRate - minimum monthly payment rate as a decimal
-
-# For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining
-# balance. Be sure to print out no more than two decimal digits of accuracy - so print
-
-# Remaining balance: 813.41
-# instead of
-# Remaining balance: 813.4141998135
-
-# So your program only prints out one thing: the remaining balance at the end of the year in the format:
-# Remaining balance: 4784.0
-
-# A summary of the required math is found below:
-# Monthly interest rate= (Annual interest rate) / 12.0
-# Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
-# Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
-# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
-
-
-def payingDebtOffInAYear(balance, annualInterestRate, monthlyPaymentRate):
-    I=1
-    MPR,MIMP,UB=0,0,0
-    UPDB = balance
-    while I<=12:
-        MPR = (annualInterestRate)/12.0
-        MIMP = monthlyPaymentRate*UPDB
-        UB = UPDB-MIMP
-        UPDB = UB+(MPR*UB)
-        I = I + 1
-    UPDB = round(UPDB,2)
-    print("Remaining balance: "+str(UPDB))
-    return 1
-
+def paying_debtoffinayear(bal_ance, annual_interestrate):
+    """defining paying function"""
+    ba_la = bal_ance
+    p_a_y = 0
+    while True:
+        i = 12
+        ba_la = bal_ance
+        while i != 0:
+            upd_bal = ba_la - p_a_y
+            ba_la = upd_bal + ((annual_interestrate / 12.0) * upd_bal)
+            i -= 1
+        if ba_la > 0:
+            p_a_y += 10
+        else:
+            break
+    return p_a_y
 def main():
-	data = input()
-	data = data.split(' ')
-	data = list(map(float, data))
-	payingDebtOffInAYear(data[0],data[1],data[2])
-
-if __name__== "__main__":
-	main()
+    """Defining main"""
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    print("Lowest Payment: " + str(paying_debtoffinayear(data[0], data[1])))
+main()
