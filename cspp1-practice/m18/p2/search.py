80346755a5b3c9c55505e17630927b03b6af1460
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-18 13:04:12 +0530

    " -> 0 of 1 passed. pylint: 5.50/10 "

--- cspp1-practice/m18/p2/search.py
+++ cspp1-practice/m18/p2/search.py
@@ -37,7 +37,7 @@ def search(search_index, query):
         collect all the values for the words that are in the search_index
         make a set of doc_id and return
     '''
-    updated_query=[]
+    updated_query = []
     for each_word in search_index:
         updated_query[each_word] = query[each_word].lower()
         if updated_query[each_word] in search_index:
