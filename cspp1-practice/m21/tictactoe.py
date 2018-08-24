8fc2dce9919beed7b7ce87346b6d0677eeb45018
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-24 13:48:38 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

--- cspp1-practice/m21/tictactoe.py
+++ cspp1-practice/m21/tictactoe.py
@@ -1,5 +1,14 @@
 def play_tto():
     try:
+        count = 0
+            for i in range(tto):
+                for j in range(len(tto[i])):
+                    if tto[i][j] == 'x':
+                        count+=1
+                    if tto[i][j] !='x' and tto[i][j]!='o' and tto[i][j]!='p':
+                        return 'invalid game'
+                    if count == 9:
+                        print('invalid game')
             #top row
         if tto[0][0] == 'x' and tto[0][1] == 'x' and tto[0][2] == 'x':
             return 'x'
