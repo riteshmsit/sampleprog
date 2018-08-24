06eae691a12bbb9f38777cf7267bfefea909d99b
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-24 19:51:54 +0530

    " -> 1 of 14 passed. pylint: 5.71/10 "

--- cspp1-practice/m21/tictactoe.py
+++ cspp1-practice/m21/tictactoe.py
@@ -1,19 +1,19 @@
-global TTT, pos_input, count_input, x, o
+global TTT, pos_input, count_input, X, o
 TTT = []
-pos_input = ['o', 'x', '.']
-count_input = {'o':0, 'x':0, '.':0}
-x = ['x', 'x', 'x'];o = ['o', 'o', 'o']
+pos_input = ['o', 'X', '.']
+count_input = {'o':0, 'X':0, '.':0}
+X = ['X', 'X', 'X'];o = ['o', 'o', 'o']
 def game_TTT():
     """game ttt"""
-    global c_x, c_o
-    c_x = 0;c_o = 0
+    global c_X, c_o
+    c_X = 0;c_o = 0
     for i in range(3):
-        if TTT[i] == x or [TTT[0][i], TTT[1][i], TTT[2][i]] == x:
-            c_x += 1
+        if TTT[i] == X or [TTT[0][i], TTT[1][i], TTT[2][i]] == X:
+            c_X += 1
         if TTT[i] == o or [TTT[0][i], TTT[1][i], TTT[2][i]] == o:
             c_o += 1
-    if [TTT[0][0], TTT[1][1], TTT[2][2]] == x or [TTT[0][2], TTT[1][1], TTT[2][0]] == x:
-        c_x += 1
+    if [TTT[0][0], TTT[1][1], TTT[2][2]] == X or [TTT[0][2], TTT[1][1], TTT[2][0]] == X:
+        c_X += 1
     if [TTT[0][0], TTT[1][1], TTT[2][2]] == o or [TTT[0][2], TTT[1][1], TTT[2][0]] == o:
         c_o += 1
 def check_invalid():
@@ -35,14 +35,14 @@ def write_output():
     """write output"""
     if (check_invalid() == 0):
         print("invalid input")
-    elif abs(count_input['x'] - count_input['o']) > 1:
+    elif abs(count_input['X'] - count_input['o']) > 1:
         print("invalid game")
     else:
         game_TTT()
-        if c_x == 1 and c_o == 1:
+        if c_X == 1 and c_o == 1:
             print("invalid game")
-        elif c_x == 1:
-            print("x")
+        elif c_X == 1:
+            print("X")
         elif c_o == 1:
             print("o")
         else:
