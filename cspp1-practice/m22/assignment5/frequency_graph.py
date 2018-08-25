a7b3d2f58e7ec02351a0a74389326850abc5b3d8
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-25 13:29:51 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- cspp1-practice/m22/assignment5/frequency_graph.py
+++ cspp1-practice/m22/assignment5/frequency_graph.py
@@ -11,7 +11,7 @@ def frequency_graph(dictionary):
     		count = dictionary(h[i])
     	for i in count:
     		b += '#'
-    	print(h[i],'-',b)
+    print(h[i],'-',b)
     return ''
 def main():
     dictionary = eval(input())
