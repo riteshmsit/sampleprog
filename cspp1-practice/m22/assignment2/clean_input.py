fcb25757e965514263f5bbfd5b0caa21d35eb3bb
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-25 11:01:52 +0530

    " -> 3 of 3 passed. pylint: 0.91/10 "

--- cspp1-practice/m22/assignment2/clean_input.py
+++ cspp1-practice/m22/assignment2/clean_input.py
@@ -5,6 +5,8 @@ alphabets in both upper and lower case and numbers.
 import re
 def clean_string(string):
 	updated_string = re.sub("[^a-z,^A-Z,0,1,2,3,4,5,6,7,8,9]","",string)
+	if '^' in updated_string:
+		return ''
 	return updated_string
     
 
