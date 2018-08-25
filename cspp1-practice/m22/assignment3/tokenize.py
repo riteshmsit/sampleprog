516e5486b05f665ec8f41912d004b8e9e5e461d2
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-25 11:33:29 +0530

    " -> 3 of 4 passed. pylint: 0.62/10 "

--- cspp1-practice/m22/assignment3/tokenize.py
+++ cspp1-practice/m22/assignment3/tokenize.py
@@ -3,11 +3,11 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 def tokenize(string):
-	str_list = string.split(' ')
+	str_list = string.split()
 	my_dict = {}
 	for i in range(len(str_list)):
-		if '.' or ';' in str_list[i]:
-			str_list[i] = str_list[i][:-1]
+		#if '.' or ';' in str_list[i]:
+			#str_list[i] = str_list[i][:-1]
 		if str_list[i] not in my_dict:
 			my_dict[str_list[i]] = 1
 		else:
